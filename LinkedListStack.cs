4df20690f9c43074887b16de9826f4d1fdb09552
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-29 23:50:44 +0530

    Stack using linkedList in push method

---
+++ LinkedListStack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    public class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.top == null)
+                newNode.next = null;
+            else
+                newNode.next = this.top;
+            this.top = newNode;
+            Console.WriteLine("{0} pushed to stack ", data);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace StackAndQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Stack and Queue Program");
+
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            linkedListStack.Display();
         }
     }
 }
